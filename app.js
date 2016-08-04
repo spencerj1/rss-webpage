507e568503a58f5eff27129a931f0c2226f43708
James Spencer <spencerj1@wit.edu>
2016-08-04 00:13:15 -0400

    rss

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ res.status(200).sendFile('./articletemplate.html', { root: __dirname })
 });
 
 app.get('/rss', function (req, res) { 
-	rssfeed.getfeed('http://www.bitpipe.com/data/bpXchange?b=ka_bp_manageit&d=31&f=rss&u=rss');
+	rssfeed.getfeed('http://www.bitpipe.com/data/bpXchange?b=ka_bp_manageit&d=31&f=rss&u=rss', function(){
+		
+	});
 });
 
 app.use(function (err, req, res, next) {
